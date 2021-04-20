e109dd52ae51c5618d22cb5a9551a1c5e7cf5916
Curso <Curso@PORT-Z7.ipartekaula.com>
2021-04-20 13:33:17 +0200

    (Corrección de un warning)

--- Sintaxis/src/com/ipartek/formacion/Conversiones.java
+++ Sintaxis/src/com/ipartek/formacion/Conversiones.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Conversiones {
 
 	public static void main(String[] args) {
-		//convertirDeStringAOtros();
+		convertirDeStringAOtros();
 		
 		convertirDeOtrosAString();
 	}
