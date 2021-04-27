db4be439b2087a38f92ae17ee3828ff4698d4232
Curso <Curso@PORT-Z7.ipartekaula.com>
2021-04-27 10:57:49 +0200

    toString y protected para X y Y_POR_DEFECTO en Punto

--- Sintaxis/src/com/ipartek/formacion/poo/Punto.java
+++ Sintaxis/src/com/ipartek/formacion/poo/Punto.java
@@ -1,6 +1,10 @@
 package com.ipartek.formacion.poo;
 
 public class Punto {
+	protected static final int Y_POR_DEFECTO = 0;
+
+	protected static final int X_POR_DEFECTO = 0;
+
 	private int x, y;
 
 	// Variable estática "de clase"
@@ -9,12 +13,16 @@ public class Punto {
 	public Punto(int x, int y) {
 		setX(x);
 		setY(y);
-		
+
 		System.out.println("Se ha creado un objeto " + aTexto() + ". Vamos " + ++contador + " objetos");
 	}
 
+	public Punto(Punto punto) {
+		this(punto.x, punto.y);
+	}
+
 	public Punto() {
-		this(0, 0);
+		this(X_POR_DEFECTO, Y_POR_DEFECTO);
 	}
 
 	public int getX() {
@@ -36,15 +44,21 @@ public class Punto {
 	public String aTexto() {
 		return String.format("P(%s,%s)", x, y);
 	}
-	
+
 	public Punto sumar(Punto p) {
-		//return new Punto(x + p.x, y + p.y);
+		// return new Punto(x + p.x, y + p.y);
 		return sumar(this, p);
 	}
-	
+
 	// Método estático "de clase"
 	public static Punto sumar(Punto p1, Punto p2) {
 		return new Punto(p1.x + p2.x, p1.y + p2.y);
-		//return p1.sumar(p2);
+		// return p1.sumar(p2);
 	}
+
+	@Override
+	public String toString() {
+		return "Punto [x=" + x + ", y=" + y + "]";
+	}
+
 }
