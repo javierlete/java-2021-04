077c0a0848f52b310b0f016adeb97cd40662aa05
Curso <Curso@PORT-Z7.ipartekaula.com>
2021-04-29 10:53:42 +0200

    Dirección de la biblioteca de SQLite para JDBC

--- BasesDeDatos/src/com/ipartek/formacion/jdbc/EjemploJdbc.java
+++ BasesDeDatos/src/com/ipartek/formacion/jdbc/EjemploJdbc.java
@@ -8,6 +8,8 @@ import java.sql.Statement;
 
 // CRUD: Create, Retrieve, Update, Delete
 
+// https://repo1.maven.org/maven2/org/xerial/sqlite-jdbc/3.34.0/sqlite-jdbc-3.34.0.jar
+
 public class EjemploJdbc {
 
 	public static void main(String[] args) throws SQLException {
