40d0a27d6ee17af797b8fe694ab1fc380d8932f8
Curso <Curso@PORT-Z7.ipartekaula.com>
2021-04-30 10:47:42 +0200

    Gestión de excepciones en la aplicación

--- BasesDeDatos/src/com/ipartek/formacion/jdbc/clientes/presentacion/consola/Presentacion.java
+++ BasesDeDatos/src/com/ipartek/formacion/jdbc/clientes/presentacion/consola/Presentacion.java
@@ -3,31 +3,44 @@ package com.ipartek.formacion.jdbc.clientes.presentacion.consola;
 import java.util.ArrayList;
 
 import com.ipartek.formacion.jdbc.clientes.entidades.Cliente;
-
+import com.ipartek.formacion.jdbc.clientes.accesodatos.AccesoDatosException;
 import com.ipartek.formacion.jdbc.clientes.accesodatos.DaoCliente;
 
 public class Presentacion {
 
 	public static void main(String[] args) {
-		Cliente cliente = DaoCliente.obtenerPorId(2);
+		try {
+			Cliente cliente = DaoCliente.obtenerPorId(2);
 
-		System.out.println(cliente);
+			System.out.println(cliente);
+			System.out.println(cliente.getNombre());
 
-		mostrarTodos();
-		
-		cliente = DaoCliente.insertar(new Cliente(null, "Juanes", "Teban"));
+			mostrarTodos();
+			
+			cliente = DaoCliente.insertar(new Cliente(null, "Juanes", "Teban"));
 
-		System.out.println("INSERTADO -> " + cliente);
-		
-		mostrarTodos();
-		
-		DaoCliente.modificar(new Cliente(cliente.getId(), "Modificado", "Modificadez"));
-		
-		mostrarTodos();
-		
-		DaoCliente.borrar(cliente.getId());
-		
-		mostrarTodos();
+			System.out.println("INSERTADO -> " + cliente);
+			
+			mostrarTodos();
+			
+			DaoCliente.modificar(new Cliente(cliente.getId(), "Modificado", "Modificadez"));
+			
+			mostrarTodos();
+			
+			DaoCliente.borrar(cliente.getId());
+			
+			mostrarTodos();
+		} catch(AccesoDatosException ade) {
+			System.out.println("Ha habido un error accediendo a la base de datos");
+			System.out.println(ade.getMessage());
+			
+			if(ade.getCause()!=null) {
+				System.out.println(ade.getCause().getMessage());
+			}
+		} catch (Exception e) {
+			System.out.println("Error no esperado");
+			System.out.println(e.getMessage());
+		}
 	}
 
 	private static void mostrarTodos() {
