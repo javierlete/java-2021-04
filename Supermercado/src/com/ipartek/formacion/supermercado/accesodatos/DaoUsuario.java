98b6e1a78d01844d90094e4fbf5aaeb912291957
Curso <Curso@LAPTOP-6ETV3G67.ipartekaula.com>
2021-05-04 10:08:36 +0200

    Ejemplo DAO básico completo

--- Supermercado/src/com/ipartek/formacion/supermercado/accesodatos/DaoUsuario.java
+++ Supermercado/src/com/ipartek/formacion/supermercado/accesodatos/DaoUsuario.java
@@ -11,8 +11,10 @@ import com.ipartek.formacion.supermercado.entidades.Usuario;
 
 public class DaoUsuario {
 	// Datos para personalizar qué base de datos vamos a utilizar
-	private static final String JDBC_DRIVER = "org.sqlite.JDBC"; // Driver de la base de datos (lo indica cada fabricante)
-	private static final String URL = "jdbc:sqlite:supermercado.sqlite3"; // supermercado.sqlite3 debe estar en el raíz del proyecto
+	private static final String JDBC_DRIVER = "org.sqlite.JDBC"; // Driver de la base de datos (lo indica cada
+																	// fabricante)
+	private static final String URL = "jdbc:sqlite:supermercado.sqlite3"; // supermercado.sqlite3 debe estar en el raíz
+																			// del proyecto
 	private static final String USUARIO_BDD = ""; // Usuario de la base de datos si lo necesita
 	private static final String PASSWORD_BDD = "";// Contraseña de la base de datos si lo necesita
 
@@ -20,11 +22,12 @@ public class DaoUsuario {
 	// Personalizar tablas y campos
 	private static final String SQL_SELECT = "SELECT id, email, password, nombre, apellidos FROM usuarios";
 	private static final String SQL_SELECT_ID = SQL_SELECT + " WHERE id=?";
-//	private static final String SQL_INSERT = "INSERT INTO usuarios (email, password, nombre, apellidos) VALUES (?, ?, ?, ?)";
-//	private static final String SQL_UPDATE = "UPDATE usuarios SET email=?, password=?, nombre=?, apellidos=? WHERE id=?";
-//	private static final String SQL_DELETE = "DELETE FROM usuarios WHERE id=?";
+	private static final String SQL_INSERT = "INSERT INTO usuarios (email, password, nombre, apellidos) VALUES (?, ?, ?, ?)";
+	private static final String SQL_UPDATE = "UPDATE usuarios SET email=?, password=?, nombre=?, apellidos=? WHERE id=?";
+	private static final String SQL_DELETE = "DELETE FROM usuarios WHERE id=?";
 
-	// Bloque que a veces es necesario si después de tener la cadena de conexión correcta (URL)
+	// Bloque que a veces es necesario si después de tener la cadena de conexión
+	// correcta (URL)
 	// y referenciado el driver de JDBC, sigue dando "unsuitable driver"
 	// Totalmente genérico. No hay que personalizar
 	static {
@@ -34,7 +37,7 @@ public class DaoUsuario {
 			throw new AccesoDatosException("No se ha encontrado la clase del driver JDBC " + JDBC_DRIVER);
 		}
 	}
-	
+
 	// Totalmente genérico. No hay que personalizar
 	private static Connection obtenerConexion() {
 		try {
@@ -54,7 +57,8 @@ public class DaoUsuario {
 			ArrayList<Usuario> usuarios = new ArrayList<>();
 
 			while (rs.next()) {
-				// Usar el constructor más grande de nuestra clase para capturar todos los campos 
+				// Usar el constructor más grande de nuestra clase para capturar todos los
+				// campos
 				// de la tabla en los propios campos del objeto
 				usuarios.add(new Usuario(rs.getInt("id"), rs.getString("email"), rs.getString("password"),
 						rs.getString("nombre"), rs.getString("apellidos")));
@@ -66,7 +70,7 @@ public class DaoUsuario {
 		}
 	}
 
-	// Debemos cambiar Usuario por el que represente nuestros datos 
+	// Debemos cambiar Usuario por el que represente nuestros datos
 	public static Usuario obtenerPorId(Integer id) {
 		try (Connection con = obtenerConexion(); PreparedStatement ps = con.prepareStatement(SQL_SELECT_ID);) {
 
@@ -77,7 +81,8 @@ public class DaoUsuario {
 			Usuario usuario = null;
 
 			if (rs.next()) {
-				// Usar el constructor más grande de nuestra clase para capturar todos los campos 
+				// Usar el constructor más grande de nuestra clase para capturar todos los
+				// campos
 				// de la tabla en los propios campos del objeto
 				usuario = new Usuario(rs.getInt("id"), rs.getString("email"), rs.getString("password"),
 						rs.getString("nombre"), rs.getString("apellidos"));
@@ -86,6 +91,49 @@ public class DaoUsuario {
 			return usuario;
 		} catch (SQLException e) {
 			throw new AccesoDatosException("Ha habido un error al obtener el registro cuyo id es " + id, e);
+		}
+	}
+
+	// Debemos cambiar Usuario por el que represente nuestros datos
+	public static void insertar(Usuario usuario) {
+		try (Connection con = obtenerConexion(); PreparedStatement ps = con.prepareStatement(SQL_INSERT);) {
+
+			ps.setString(1, usuario.getEmail());
+			ps.setString(2, usuario.getPassword());
+			ps.setString(3, usuario.getNombre());
+			ps.setString(4, usuario.getApellidos());
+
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw new AccesoDatosException("Ha habido un error al insertar el registro", e);
+		}
+	}
+
+	// Debemos cambiar Usuario por el que represente nuestros datos
+	public static void modificar(Usuario usuario) {
+		try (Connection con = obtenerConexion(); PreparedStatement ps = con.prepareStatement(SQL_UPDATE);) {
+
+			ps.setString(1, usuario.getEmail());
+			ps.setString(2, usuario.getPassword());
+			ps.setString(3, usuario.getNombre());
+			ps.setString(4, usuario.getApellidos());
+			ps.setInt(5, usuario.getId());
+
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw new AccesoDatosException("Ha habido un error al modificar el registro", e);
+		}
+	}
+
+	// Debemos cambiar Usuario por el que represente nuestros datos
+	public static void borrar(Integer id) {
+		try (Connection con = obtenerConexion(); PreparedStatement ps = con.prepareStatement(SQL_DELETE);) {
+
+			ps.setInt(1, id);
+
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw new AccesoDatosException("Ha habido un error al modificar el registro", e);
 		}
 	}
 }
--- Supermercado/src/com/ipartek/formacion/supermercado/presentacionconsola/PresentacionConsola.java
+++ Supermercado/src/com/ipartek/formacion/supermercado/presentacionconsola/PresentacionConsola.java
@@ -6,12 +6,30 @@ import com.ipartek.formacion.supermercado.entidades.Usuario;
 public class PresentacionConsola {
 
 	public static void main(String[] args) {
+		Usuario u = DaoUsuario.obtenerPorId(1);
+		System.out.println(u);
+
+		mostrarTodos();
+		
+		DaoUsuario.insertar(new Usuario(null, "email1@email.net", "contraseña", "Prueba", "Pruebez"));
+		
+		mostrarTodos();
+		
+		DaoUsuario.modificar(new Usuario(4, "modificado@email.net", "cambiadacontra", "Modificado", "Modificadez"));
+		
+		mostrarTodos();
+		
+		DaoUsuario.borrar(4);
+		
+		mostrarTodos();
+	}
+
+	private static void mostrarTodos() {
+		System.out.println("LISTADO");
+		
 		for(Usuario usuario: DaoUsuario.obtenerTodos()) {
 			System.out.println(usuario);
 		}
-		
-		Usuario u = DaoUsuario.obtenerPorId(1);
-		System.out.println(u);
 	}
 
 }
