be9f5720215142b01e43bad1d973ed03fc5ce4d3
Curso <Curso@LAPTOP-6ETV3G67.ipartekaula.com>
2021-05-04 09:51:45 +0200

    Comentarios para personalizar el DAO

--- Supermercado/src/com/ipartek/formacion/supermercado/accesodatos/DaoUsuario.java
+++ Supermercado/src/com/ipartek/formacion/supermercado/accesodatos/DaoUsuario.java
@@ -10,11 +10,14 @@ import java.util.ArrayList;
 import com.ipartek.formacion.supermercado.entidades.Usuario;
 
 public class DaoUsuario {
-	private static final String JDBC_DRIVER = "org.sqlite.JDBC";
-	private static final String URL = "jdbc:sqlite:supermercado.sqlite3";
-	private static final String USUARIO_BDD = "";
-	private static final String PASSWORD_BDD = "";
+	// Datos para personalizar qué base de datos vamos a utilizar
+	private static final String JDBC_DRIVER = "org.sqlite.JDBC"; // Driver de la base de datos (lo indica cada fabricante)
+	private static final String URL = "jdbc:sqlite:supermercado.sqlite3"; // supermercado.sqlite3 debe estar en el raíz del proyecto
+	private static final String USUARIO_BDD = ""; // Usuario de la base de datos si lo necesita
+	private static final String PASSWORD_BDD = "";// Contraseña de la base de datos si lo necesita
 
+	// Consultas que vamos a lanzar a la base de datos
+	// Personalizar tablas y campos
 	private static final String SQL_SELECT = "SELECT id, email, password, nombre, apellidos FROM usuarios";
 	private static final String SQL_SELECT_ID = SQL_SELECT + " WHERE id=?";
 //	private static final String SQL_INSERT = "INSERT INTO usuarios (email, password, nombre, apellidos) VALUES (?, ?, ?, ?)";
@@ -23,14 +26,16 @@ public class DaoUsuario {
 
 	// Bloque que a veces es necesario si después de tener la cadena de conexión correcta (URL)
 	// y referenciado el driver de JDBC, sigue dando "unsuitable driver"
+	// Totalmente genérico. No hay que personalizar
 	static {
 		try {
 			Class.forName(JDBC_DRIVER);
 		} catch (ClassNotFoundException e) {
-			throw new AccesoDatosException("No se ha encontrado la clase del driver JDBC de SQLite");
+			throw new AccesoDatosException("No se ha encontrado la clase del driver JDBC " + JDBC_DRIVER);
 		}
 	}
 	
+	// Totalmente genérico. No hay que personalizar
 	private static Connection obtenerConexion() {
 		try {
 			return DriverManager.getConnection(URL, USUARIO_BDD, PASSWORD_BDD);
@@ -39,14 +44,18 @@ public class DaoUsuario {
 		}
 	}
 
+	// Debemos cambiar Usuario por el que represente nuestros datos
 	public static ArrayList<Usuario> obtenerTodos() {
 		try (Connection con = obtenerConexion();
 				PreparedStatement ps = con.prepareStatement(SQL_SELECT);
 				ResultSet rs = ps.executeQuery()) {
 
+			// Debemos cambiar Usuario por el que represente nuestros datos
 			ArrayList<Usuario> usuarios = new ArrayList<>();
 
 			while (rs.next()) {
+				// Usar el constructor más grande de nuestra clase para capturar todos los campos 
+				// de la tabla en los propios campos del objeto
 				usuarios.add(new Usuario(rs.getInt("id"), rs.getString("email"), rs.getString("password"),
 						rs.getString("nombre"), rs.getString("apellidos")));
 			}
@@ -57,6 +66,7 @@ public class DaoUsuario {
 		}
 	}
 
+	// Debemos cambiar Usuario por el que represente nuestros datos 
 	public static Usuario obtenerPorId(Integer id) {
 		try (Connection con = obtenerConexion(); PreparedStatement ps = con.prepareStatement(SQL_SELECT_ID);) {
 
@@ -67,13 +77,15 @@ public class DaoUsuario {
 			Usuario usuario = null;
 
 			if (rs.next()) {
+				// Usar el constructor más grande de nuestra clase para capturar todos los campos 
+				// de la tabla en los propios campos del objeto
 				usuario = new Usuario(rs.getInt("id"), rs.getString("email"), rs.getString("password"),
 						rs.getString("nombre"), rs.getString("apellidos"));
 			}
 
 			return usuario;
 		} catch (SQLException e) {
-			throw new AccesoDatosException("Ha habido un error al obtener el usuario cuyo id es " + id, e);
+			throw new AccesoDatosException("Ha habido un error al obtener el registro cuyo id es " + id, e);
 		}
 	}
 }
