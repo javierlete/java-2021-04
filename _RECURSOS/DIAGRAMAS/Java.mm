ae94f462d4edcec23596dafe144b812b34709ff2
Curso <Curso@LAPTOP-6ETV3G67.ipartekaula.com>
2021-05-07 12:11:09 +0200

    Diagramas de tecnologías Java con Spring

---
+++ _RECURSOS/DIAGRAMAS/Java.mm
@@ -0,0 +1,120 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Java" FOLDED="false" ID="ID_939404711" CREATED="1620378489911" MODIFIED="1620378495435" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Herramientas" POSITION="left" ID="ID_479139113" CREATED="1620381175205" MODIFIED="1620381177356">
+<edge COLOR="#7c0000"/>
+<node TEXT="Maven" ID="ID_1198713856" CREATED="1620381177521" MODIFIED="1620381178474">
+<node TEXT="Spring Boot" ID="ID_71707355" CREATED="1620381212120" MODIFIED="1620381214256"/>
+</node>
+</node>
+<node TEXT="Entidades (JPA, Lombok)" POSITION="left" ID="ID_338638786" CREATED="1620380923624" MODIFIED="1620380990012">
+<edge COLOR="#00ffff"/>
+<node TEXT="Presentación" ID="ID_1922763199" CREATED="1620380898984" MODIFIED="1620380923670">
+<node TEXT="Thymeleaf" ID="ID_1093926917" CREATED="1620381151913" MODIFIED="1620381153868">
+<node TEXT="Genera HTML" ID="ID_1213278855" CREATED="1620381417950" MODIFIED="1620381419992"/>
+</node>
+<node TEXT="Spring MVC" ID="ID_1329992440" CREATED="1620380933822" MODIFIED="1620380936723"/>
+</node>
+<node TEXT="Lógica de negocio" ID="ID_762869837" CREATED="1620380913781" MODIFIED="1620380923685">
+<node TEXT="Spring Framework" ID="ID_1226527824" CREATED="1620380938597" MODIFIED="1620380942778"/>
+</node>
+<node TEXT="Acceso a datos" ID="ID_1224666229" CREATED="1620380918870" MODIFIED="1620380923695">
+<node TEXT="Spring Data JPA" ID="ID_1245906547" CREATED="1620381031934" MODIFIED="1620381035788"/>
+</node>
+</node>
+<node TEXT="Cliente" POSITION="right" ID="ID_524155435" CREATED="1620381278076" MODIFIED="1620381279231">
+<edge COLOR="#007c00"/>
+<node TEXT="Angular" ID="ID_1572299957" CREATED="1620381279516" MODIFIED="1620381296085">
+<font BOLD="true"/>
+<node TEXT="Google" ID="ID_1478094095" CREATED="1620381320705" MODIFIED="1620381322249"/>
+</node>
+<node TEXT="React" ID="ID_1184032657" CREATED="1620381282676" MODIFIED="1620381284044">
+<node TEXT="Facebook" ID="ID_315289759" CREATED="1620381323922" MODIFIED="1620381325765"/>
+</node>
+<node TEXT="Vue" ID="ID_580144207" CREATED="1620381284362" MODIFIED="1620381288272">
+<node TEXT="Evan You" ID="ID_836331459" CREATED="1620381326620" MODIFIED="1620381328682"/>
+</node>
+<node TEXT="Web Components" ID="ID_1398789343" CREATED="1620381455903" MODIFIED="1620381458357">
+<node TEXT="WHATWG Standard" ID="ID_1781128812" CREATED="1620381461293" MODIFIED="1620381473191"/>
+</node>
+<node TEXT="..." ID="ID_1201564846" CREATED="1620381291558" MODIFIED="1620381292461"/>
+</node>
+<node TEXT="Servidor" POSITION="right" ID="ID_781214742" CREATED="1620381276095" MODIFIED="1620381277568">
+<edge COLOR="#00007c"/>
+<node TEXT="Entidades (JPA, Lombok)" ID="ID_328490608" CREATED="1620380923624" MODIFIED="1620381276137">
+<node TEXT="REST" ID="ID_900796235" CREATED="1620381263305" MODIFIED="1620381264495">
+<node TEXT="Spring REST" ID="ID_292648648" CREATED="1620381268076" MODIFIED="1620381270472"/>
+</node>
+<node TEXT="Lógica de negocio" ID="ID_1658495261" CREATED="1620380913781" MODIFIED="1620380923685">
+<node TEXT="Spring Framework" ID="ID_1401767629" CREATED="1620380938597" MODIFIED="1620380942778"/>
+</node>
+<node TEXT="Acceso a datos" ID="ID_1499840186" CREATED="1620380918870" MODIFIED="1620380923695">
+<node TEXT="Spring Data JPA" ID="ID_1859436648" CREATED="1620381031934" MODIFIED="1620381035788"/>
+</node>
+</node>
+</node>
+</node>
+</map>
