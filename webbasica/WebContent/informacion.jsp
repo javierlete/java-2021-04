5d419e33691df8bc8c13b041f0fcb4d0efc99e63
Curso <Curso@LAPTOP-6ETV3G67.ipartekaula.com>
2021-05-12 12:23:25 +0200

    Información recibida de navegador

---
+++ webbasica/WebContent/informacion.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Información</title>
+</head>
+<body>
+
+<h1>IP desde la que te has conectado: ${ip}</h1>
+<h1>Ruta extra que has enviado: ${ruta}</h1>
+<h1>${idioma}</h1>
+<h1>${navegador}</h1>
+<h1>${anterior}</h1>
+
+<a href="/webbasica/info/prueba/de/ruta">Prueba de ruta</a>
+
+</body>
+</html>
---
+++ webbasica/src/com/ipartek/formacion/webbasica/Informacion.java
@@ -0,0 +1,39 @@
+package com.ipartek.formacion.webbasica;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/info/*") // "/*" necesario para pathinfo
+public class Informacion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Recibimos información de petición
+		String strIp = request.getRemoteAddr();
+		String strRuta = request.getPathInfo();
+		
+		String strNavegador = request.getHeader("user-agent");
+		String strAnterior = request.getHeader("referer");
+		String strIdioma = request.getHeader("accept-language");
+		
+		// Preparamos información para la pantalla
+		request.setAttribute("ip", strIp);
+		request.setAttribute("ruta", strRuta);
+		request.setAttribute("navegador", strNavegador);
+		request.setAttribute("anterior", strAnterior);
+		request.setAttribute("idioma", strIdioma);
+		
+		// Llamamos a la siguiente pantalla
+		request.getRequestDispatcher("/informacion.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
