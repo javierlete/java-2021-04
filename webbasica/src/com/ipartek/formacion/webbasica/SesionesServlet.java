1eada42e12fb7e3d3b6298a4dd78e97490e4d5b1
Curso <Curso@LAPTOP-6ETV3G67.ipartekaula.com>
2021-05-18 08:51:20 +0200

    Retoques en SesionesServlet

--- webbasica/src/com/ipartek/formacion/webbasica/SesionesServlet.java
+++ webbasica/src/com/ipartek/formacion/webbasica/SesionesServlet.java
@@ -1,30 +1,33 @@
 package com.ipartek.formacion.webbasica;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/sesiones")
 public class SesionesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// Almacenamos en request (sólo se conserva con el request dispatcher
+		// Almacenamos en request (sólo se conserva con el request dispatcher (forward)
 		request.setAttribute("peticion", "Dato de petición");
 		
 		// Almacenamos en sesión (se conserva hasta 30 minutos de inactividad)
-		request.getSession().setAttribute("sesion", "Dato de sesión");
+		HttpSession session = request.getSession();
+		session.setAttribute("sesion", "Dato de sesión");
 		
-		// Invalida todos los objetos de sesión asociada al navegador
-//		request.getSession().invalidate();
+		// Invalida todos los atributos de sesión asociada al navegador
+//		session.invalidate();
 		
-		// No cambia la URL del navegador y conserva tanto request como sesión
+		// No cambia la URL del navegador y conserva tanto request como session
 //		request.getRequestDispatcher("sesiones.jsp").forward(request, response);
 
-		// Cambia la URL del navegador y se conserva la sesión pero no la request 
+		// Cambia la URL del navegador y se conserva la session pero no la request 
 		response.sendRedirect("sesiones.jsp");
 	}
 
