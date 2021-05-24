c4db09d1f1d265d19e5c1bf413a28110e31067af
Curso <Curso@LAPTOP-6ETV3G67.ipartekaula.com>
2021-05-24 12:29:06 +0200

    Versión de listado de productos con DAO

--- tiendaonline/src/main/java/com/ipartek/formacion/tiendaonline/controladores/admin/ListadoProductosServlet.java
+++ tiendaonline/src/main/java/com/ipartek/formacion/tiendaonline/controladores/admin/ListadoProductosServlet.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ipartek.formacion.tiendaonline.controladores.config.Config;
 import com.ipartek.formacion.tiendaonline.modelos.Producto;
 
 @WebServlet("/admin/productos/listado")
@@ -23,22 +24,25 @@ public class ListadoProductosServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		List<Producto> productos = new ArrayList<>();
+//		List<Producto> productos = new ArrayList<>();
+//		
+//		try (Connection con = DriverManager.getConnection("jdbc:sqlite:" + getServletContext().getRealPath("/WEB-INF/tiendaonline.sqlite3"));
+//				Statement st = con.createStatement();
+//				ResultSet rs = st.executeQuery("SELECT * FROM producto")) {
+//			while(rs.next()) {
+//				productos.add(new Producto(rs.getInt("id"), rs.getString("nombre"), new BigDecimal(rs.getString("precio").replace(",", "."))));
+//			}
 		
-		try (Connection con = DriverManager.getConnection("jdbc:sqlite:" + getServletContext().getRealPath("/WEB-INF/tiendaonline.sqlite3"));
-				Statement st = con.createStatement();
-				ResultSet rs = st.executeQuery("SELECT * FROM producto")) {
-			while(rs.next()) {
-				productos.add(new Producto(rs.getInt("id"), rs.getString("nombre"), new BigDecimal(rs.getString("precio").replace(",", "."))));
-			}
+			// Todos los comentarios de este método se sustituyen por esta línea
+			List<Producto> productos = Config.daoProducto.select();
 			
 			request.setAttribute("productos", productos);
 			
 			request.getRequestDispatcher("/admin/productos/listado.jsp").forward(request, response);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
