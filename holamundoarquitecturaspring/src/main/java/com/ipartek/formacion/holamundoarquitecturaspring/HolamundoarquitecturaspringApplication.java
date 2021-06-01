642271e0f2f17313afcbc8b78b52607f7f79034b
Curso <Curso@LAPTOP-6ETV3G67.ipartekaula.com>
2021-06-01 10:44:27 +0200

    Inicio holamundoarquitecturaspring

---
+++ holamundoarquitecturaspring/src/main/java/com/ipartek/formacion/holamundoarquitecturaspring/HolamundoarquitecturaspringApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.holamundoarquitecturaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolamundoarquitecturaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolamundoarquitecturaspringApplication.class, args);
+	}
+
+}
---
+++ holamundoarquitecturaspring/src/test/java/com/ipartek/formacion/holamundoarquitecturaspring/HolamundoarquitecturaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.holamundoarquitecturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolamundoarquitecturaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
